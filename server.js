3c6cb029ee3ab12aabca4086e0b0d5761a619017
Maxx <maxhiga.hawaii@gmail.com>
2021-07-15 19:40:34 -1000

    started server

---
+++ server.js
@@ -0,0 +1,25 @@
+//server dependencies
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//mongoose connection
+
+//
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
+
+//routes
+//app.use("/api/", require("./routes/"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
