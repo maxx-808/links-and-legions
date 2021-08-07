c0e2a1afd983eec4f403c9f3c3f91c1dc130eece
Maxx <maxhiga.hawaii@gmail.com>
2021-08-07 03:00:32 -1000

    got data from backend to save to state

--- client/src/Pages/Characters.js
+++ client/src/Pages/Characters.js
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import axios from "axios";
 
 const Characters = () => {
-  const page = useState({ page: "about" });
+  const page = useState({ page: "characters" });
+  const [characters, setCharacters] = useState([]);
 
   const getChars = async (req, res) => {
-      try {
-          const all = await axios.get("/api/characters")
-          return all
+    let cancelToken = axios.CancelToken;
+    let source = cancelToken.source();
+    try {
+      const all = await axios.get("/api/characters", {
+        cancelToken: source.token,
+      });
+      setCharacters(all.data);
+    } catch (err) {
+      axios.isCancel(err)
+        ? console.log("Request cancelled")
+        : console.log("getChars", err);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      if (characters === []) {
+        getChars();
+      } else {
+        console.log("characters have been saved");
+        console.log(characters);
       }
-  }
+    } catch (err) {
+      console.log("useEffect err", err);
+    }
+  });
   return (
     <div className="page">
       <h4>Hello world</h4>
