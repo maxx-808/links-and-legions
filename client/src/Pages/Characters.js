8495b141760d0a61d0ce95298b67a85850d0a377
Maxx <maxhiga.hawaii@gmail.com>
2021-08-09 13:08:22 -1000

    adding table for characters

--- client/src/Pages/Characters.js
+++ client/src/Pages/Characters.js
@@ -6,36 +6,46 @@ const Characters = () => {
   const page = useState({ page: "characters" });
   const [characters, setCharacters] = useState([]);
 
-  const getChars = async (req, res) => {
-    let cancelToken = axios.CancelToken;
-    let source = cancelToken.source();
-    try {
-      const all = await axios.get("/api/characters", {
-        cancelToken: source.token,
-      });
-      setCharacters(all.data);
-    } catch (err) {
-      axios.isCancel(err)
-        ? console.log("Request cancelled")
-        : console.log("getChars", err);
-    }
-  };
+  // const getChars = async (req, res) => {};
 
   useEffect(() => {
-    try {
-      if (characters === []) {
-        getChars();
-      } else {
-        console.log("characters have been saved");
-        console.log(characters);
+    (async () => {
+      try {
+        let cancelToken = axios.CancelToken;
+        let source = cancelToken.source();
+        try {
+          const all = await axios.get("/api/characters", {
+            cancelToken: source.token,
+          });
+          setCharacters(all.data);
+        } catch (err) {
+          axios.isCancel(err)
+            ? console.log("Request cancelled")
+            : console.log("getChars", err);
+        }
+      } catch (err) {
+        console.log("useEffect err", err);
       }
-    } catch (err) {
-      console.log("useEffect err", err);
-    }
-  });
+    })();
+  }, []);
   return (
     <div className="page">
-      <h4>Hello world</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>stat</th>
+          </tr>
+        </thead>
+        <tbody>
+          {characters.map((character, index) => (
+            <tr key={index}>
+              <td>{character.name}</td>
+              <td>{character.stat}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
