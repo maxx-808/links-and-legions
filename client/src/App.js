9855edd30d82b397f2cfdda2b03a8d2b955d8ce7
Maxx <maxhiga.hawaii@gmail.com>
2021-08-09 22:23:04 -1000

    added new route for characterlinks page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Nav from "./Components/Navbar/Nav";
 import Landing from "./Pages/Landing";
 import About from "./Pages/About";
 import Characters from "./Pages/Characters";
+import CharacterLinks from "./Pages/CharacterLinks";
 
 function App() {
   const page = window.location.pathname;
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route path="/about" component={About} />
           <Route path="/char" component={Characters} />
+          <Route path="/char_links" component={CharacterLinks} />
         </Switch>
       </Router>
     </div>
