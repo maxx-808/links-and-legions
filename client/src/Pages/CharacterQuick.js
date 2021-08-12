c9a4ae04a3295a28999f570889cb2280b52791d2
Maxx <maxhiga.hawaii@gmail.com>
2021-08-11 17:23:29 -1000

    added quick character page

---
+++ client/src/Pages/CharacterQuick.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import getCharacters from "../Components/GetCharacters/getCharacters";
+
+const CharacterQuick = () => {
+  const page = useState({ page: "characters" });
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getCharacters();
+      setCharacters(res.data);
+    })();
+  }, []);
+  return (
+    <div className="page">
+      <table id="allTable">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th className="stat">Stat</th>
+            <th>Link</th>
+            <th>Legion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {characters.map((character, index) => (
+            <tr key={index}>
+              <td>{character.name}</td>
+              <td>{character.stat}</td>
+              <td>{character.link}</td>
+              <td>{character.legion}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CharacterQuick;
